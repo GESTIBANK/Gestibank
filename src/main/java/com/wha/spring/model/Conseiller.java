e1de7b1522b4030fb453e517c969e66ea99e51ec
GESTIBANK <fabioleotta79@gmail.com>
2019-07-15 16:48:35 +0200

    Conseiller, DemandeClient, DemandeOuverte

---
+++ src/main/java/com/wha/spring/model/Conseiller.java
@@ -0,0 +1,37 @@
+package com.wha.spring.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@DiscriminatorValue("Conseiller")
+public class Conseiller extends User implements Serializable{
+
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private int matricule;
+
+
+private List<Client> listeClients;
+private List<DemandeClient> listeDemandeClient;
+private List<DomandeOuverture> listeDemandeOuvertureAValider;
+
+
+
+
+
+}
--- src/main/java/com/wha/spring/model/User.java
+++ src/main/java/com/wha/spring/model/User.java
@@ -1,11 +1,20 @@
 package com.wha.spring.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Table;
+
+import org.springframework.context.annotation.Description;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,23 +22,36 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="UserType", discriminatorType=DiscriminatorType.STRING)
+@DiscriminatorValue("User")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "User")
-public class User {
+public class User implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
-	@Column(name = "NAME", nullable = false)
-	private String name;
+	@Column(name = "NOM", nullable = false)
+	private String nom;
 	
-	@Column(name = "USERNAME", nullable = false)
-	private String username;
+	@Column(name = "PRENOME", nullable = false)
+	private String prenome;
 	
 	@Column(name = "EMAIL", nullable = false)
 	private String email;
+	@Column(name = "ADRESSE", nullable = false)
+	private String adresse;
+	
+	@Column(name = "TELEPHONE", nullable = false)
+	private String telephone;
+	
+	@Column(name = "PSEUDO", nullable = false)
+	private String pseudo;
+	@Column(name = "MDP", nullable = false)
+	private String mdp;
 }
